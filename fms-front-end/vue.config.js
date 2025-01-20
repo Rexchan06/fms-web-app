184edfe493b7f5244342017c8c567503cd27b3f4
Rex Chan <rexchan06@gmail.com>
2025-01-20 09:16:35 +0800

    Refreshed Git index

---
+++ fms-front-end/vue.config.js
@@ -0,0 +1,24 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+
+  configureWebpack: {
+    resolve: {
+      fallback: {
+        "http": require.resolve("stream-http"),
+        "https": require.resolve("https-browserify"),
+        "util": require.resolve("util/"),
+        "stream": require.resolve("stream-browserify"),
+        "url": require.resolve("url/"),
+        "assert": require.resolve("assert/"),
+        "zlib": require.resolve("browserify-zlib")
+      }
+    }
+  }
+})
+
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/fms-web-app/'
+    : '/'
+}
